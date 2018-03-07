12879fca7c92c5e2a5c37904cede6683d84ea205
sndstudy <sanada2054tech@gmail.com>
2018-03-07 23:23:19 +0900

    GitHubAPI v4 お試し

--- src/GraphQL.js
+++ src/GraphQL.js
@@ -2,19 +2,21 @@ function test(){
 
     let request = require('request');
     const URL = "https://api.github.com/graphql"
+    const TOKEN = "GitHubのトークン"
 
     var headers = {
         "User-Agent":"sndstudy",
-        "Content-type":"application/graphql"
+        "Content-type":"application/graphql",
+        "Authorization":`bearer ${TOKEN}`
     }
 
     var options = {
         "url": URL,
         "headers": headers,
-        "body": "query {repository(owner:\"sndstudy\,name:\"til\"){createdAt}}"
+        "body": JSON.stringify({query : "{repository(owner: \"sndstudy\", name: \"til\") {createdAt}}"})
     }
 
-    request(options,(err,res,body)=>{
+    request.post(options,(err,res,body)=>{
         console.log(body);
     });
 }
